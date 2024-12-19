70f538bd24fdb704ab257a325d2baa394c51b91b
hyunjun9788 <namhyunjun9788@gmail.com>
2024-12-19 15:47:23 +0900

    fix: question 메서드로 수정

--- src/index.js
+++ src/index.js
@@ -7,30 +7,47 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-console.log('컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요')
 
 
-const randomNumber = Math.floor(Math.random() * (50 - 1) + 1)
-console.log(randomNumber)
-console.log('숫자 입력:')
 
-let count = 0
+let count = 1
+async function gamePlay() {
+    console.log('컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요')
+    const randomNumber = Math.floor(Math.random() * (50 - 1) + 1)
 
-rl.on("line", (line) => {
-    if (randomNumber > Number(line)) {
-        console.log('업')
-        count++
-    } else if (randomNumber < Number(line)) {
-        console.log('다운')
-        count++
-    } else {
-        console.log('정답!')
-        console.log(`축하합니다! ${count}번 만에 숫자를 맞추셨습니다.`)
-        rl.close();
+    console.log(randomNumber)
+
+    while (true) {
+        console.log('숫자 입력: ')
+        const userInput = await rl.question('')
+
+        count++;
+
+        if (randomNumber > userInput) {
+            console.log('업')
+        } else if (randomNumber < userInput) {
+            console.log('다운')
+        } else {
+            console.log('정답!')
+            console.log('축하합니다. 3번 만에 숫자를 맞추셨습니다.')
+
+            const playAgain = await rl.question('게임을 다시 시작하시겠습니까? (yes/no): ');
+
+            if (playAgain === 'no') {
+                console.log('게임을 종료합니다.')
+                rl.close()
+                break
+            }
+            if (playAgain === 'yes') {
+                gamePlay()
+                break
+            }
+        }
     }
+
 }
 
-)
+gamePlay()
 
 rl.on('close', () => {
     process.exit();
