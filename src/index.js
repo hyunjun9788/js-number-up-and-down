9bb3b76177077e19c3356bbde789e58ca7983c64
hyunjun9788 <namhyunjun9788@gmail.com>
2024-12-19 14:11:57 +0900

    feat: 반복 입력 및 출력 로직 구현

--- src/index.js
+++ src/index.js
@@ -8,15 +8,32 @@ const rl = readline.createInterface({
 });
 
 console.log('컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요')
+
+
 const randomNumber = Math.floor(Math.random() * (50 - 1) + 1)
 console.log(randomNumber)
 console.log('숫자 입력:')
 
-//정답이 아니면서 5회 이내일때는 계속 반복  
+let count = 0
+
 rl.on("line", (line) => {
-    rl.close();
-});
+    if (randomNumber > Number(line)) {
+        console.log('업')
+        count++
+    } else if (randomNumber < Number(line)) {
+        console.log('다운')
+        count++
+    } else {
+        console.log('정답!')
+        console.log(`축하합니다! ${count}번 만에 숫자를 맞추셨습니다.`)
+        rl.close();
+    }
+}
+
+)
 
 rl.on('close', () => {
     process.exit();
 })
+
+
