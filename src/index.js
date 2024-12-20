7d8db788eb3724747f491e1bf2f45cc7cb23f1c4
hyunjun9788 <namhyunjun9788@gmail.com>
2024-12-20 12:43:47 +0900

    feat: no 또는 yes 외 입력 시 재질문 구현

--- src/index.js
+++ src/index.js
@@ -9,19 +9,37 @@ const rl = readline.createInterface({
 
 
 
-
 async function gamePlay() {
     console.log('컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요')
     const randomNumber = Math.floor(Math.random() * (50 - 1) + 1)
     let count = 0
 
     console.log(randomNumber)
+    const prevGuess = []
+    const arrayToString = (prevGuessArr) => prevGuessArr.join(' ')
 
+    async function inputGameRestart() {
+        while (true) {
+            const playAgain = await rl.question('게임을 다시 시작하시겠습니까? (yes/no): ');
+
+            if (playAgain === 'no') {
+                console.log('게임을 종료합니다.')
+                rl.close()
+                break
+            }
+            else if (playAgain === 'yes') {
+                gamePlay()
+                break
+            }
+
+        }
+
+    }
     while (true) {
 
         console.log('숫자 입력: ')
         const userInput = await rl.question('')
-
+        prevGuess.push(userInput)
         count++;
 
         if (count > 5) {
@@ -45,27 +63,16 @@ async function gamePlay() {
         }
         if (randomNumber > userInput) {
             console.log('업')
+            console.log('이전 추측:', arrayToString(prevGuess))
+
         } else if (randomNumber < userInput) {
             console.log('다운')
         } else {
             console.log('정답!')
             console.log(`축하합니다. ${count}번 만에 숫자를 맞추셨습니다.`)
 
-            const playAgain = await rl.question('게임을 다시 시작하시겠습니까? (yes/no): ');
-
-            if (playAgain === 'no') {
-                console.log('게임을 종료합니다.')
-                rl.close()
-                break
-            }
-            else if (playAgain === 'yes') {
-                gamePlay()
-                break
-            }
-            else {
-                rl.close()
-                break
-            }
+            inputGameRestart()
+            break
         }
     }
 
