fb39fb8ef338dae1f43ed656f4c0dd00b8b05246
hyunjun9788 <namhyunjun9788@gmail.com>
2024-12-20 14:58:42 +0900

    refactor: 중복 코드 분리

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 
 import * as readline from 'node:readline/promises'
+import { arrayToString } from './utils/arrayUtils';
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -8,35 +9,51 @@ const rl = readline.createInterface({
 });
 
 
+function progressGuess(randomNumber, guess, prevGuess) {
+    if (randomNumber > Number(guess)) {
+        console.log('업')
+        console.log('이전 추측:', arrayToString(prevGuess))
+        return 'up'
+    }
 
-async function gamePlay() {
+    if (randomNumber < Number(guess)) {
+        console.log('다운')
+        console.log('이전 추측:', arrayToString(prevGuess))
+        return 'down'
+    }
+
+    console.log('정답!')
+    return 'correct'
+}
+
+async function askPlayAgain() {
+    while (true) {
+        const playAgainInput = await rl.question('게임을 다시 시작하시겠습니까? (yes/no): ');
+
+        if (playAgainInput === 'no') {
+            console.log('게임을 종료합니다.')
+            rl.close()
+            break
+        }
+        else if (playAgainInput === 'yes') {
+            playGame()
+            break
+        } else {
+            askPlayAgain()
+        }
+    }
+}
+
+async function playGame() {
     console.log('컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요')
     const randomNumber = Math.floor(Math.random() * (50 - 1) + 1)
     let count = 0
 
     console.log(randomNumber)
+
     const prevGuess = []
-    const arrayToString = (prevGuessArr) => prevGuessArr.join(' ')
 
-    async function inputGameRestart() {
-        while (true) {
-            const playAgain = await rl.question('게임을 다시 시작하시겠습니까? (yes/no): ');
-
-            if (playAgain === 'no') {
-                console.log('게임을 종료합니다.')
-                rl.close()
-                break
-            }
-            else if (playAgain === 'yes') {
-                gamePlay()
-                break
-            }
-
-        }
-
-    }
     while (true) {
-
         console.log('숫자 입력: ')
         const userInput = await rl.question('')
         prevGuess.push(userInput)
@@ -44,41 +61,21 @@ async function gamePlay() {
 
         if (count > 5) {
             console.log(`5회 초과! 숫자를 맞추지 못했습니다. 정답: ${randomNumber}`)
-            const playAgain = await rl.question('게임을 다시 시작하시겠습니까? (yes/no): ');
-
-            if (playAgain === 'no') {
-                console.log('게임을 종료합니다.')
-                rl.close()
-                break
-            }
-            else if (playAgain === 'yes') {
-                gamePlay()
-                break
-            }
-            else {
-                rl.close()
-                break
-            }
-
+            await askPlayAgain()
+            break
         }
-        if (randomNumber > userInput) {
-            console.log('업')
-            console.log('이전 추측:', arrayToString(prevGuess))
 
-        } else if (randomNumber < userInput) {
-            console.log('다운')
-        } else {
-            console.log('정답!')
+        const result = progressGuess(randomNumber, userInput, prevGuess)
+        if (result === 'correct') {
             console.log(`축하합니다. ${count}번 만에 숫자를 맞추셨습니다.`)
-
-            inputGameRestart()
+            askPlayAgain()
             break
         }
     }
-
 }
 
-gamePlay()
+
+playGame()
 
 rl.on('close', () => {
     process.exit();
